de73d2a6bf5375dcc503182c535d5c56a638e237
Canqu1 <tuanlmhe161245@fpt.edu.vn>
2024-09-25 22:33:55 +0700

    API examiner

---
+++ Library/Request/ExamDetailRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace Library.Request
+{
+    internal class ExamDetailRequest
+    {
+        public int ExamId { get; set; }
+
+        public string? ExamCode { get; set; }
+
+        public string? ExamDuration { get; set; }
+
+        public string? ExamType { get; set; }
+
+        public string? SubjectName { get; set; }
+
+        public string? ExamCreater { get; set; }
+
+        public string? HeadOfDepartment { get; set; }
+
+        public string? ExamStatus { get; set; }
+
+        public DateTime? EstimatedTimeTest { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+    }
+}
