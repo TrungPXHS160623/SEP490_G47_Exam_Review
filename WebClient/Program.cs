ee907b972c3779e889682b6504ae6a1f2f26b24d
hunglt <hunglthe160235@fpt.edu.vn>
2024-09-26 17:54:22 +0700

    update import dialog

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -45,7 +45,7 @@ namespace WebClient
                 config.SnackbarConfiguration.PreventDuplicates = false;
                 config.SnackbarConfiguration.NewestOnTop = true;
                 config.SnackbarConfiguration.ShowCloseIcon = true;
-                config.SnackbarConfiguration.VisibleStateDuration = 3000;
+                config.SnackbarConfiguration.VisibleStateDuration = 5000;
                 config.SnackbarConfiguration.HideTransitionDuration = 500;
                 config.SnackbarConfiguration.ShowTransitionDuration = 500;
                 config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
