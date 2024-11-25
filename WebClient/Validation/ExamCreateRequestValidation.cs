5319e6ec0e1e8d3bc4abaf5a831890a595b4a8d6
HungLee692002 <hungle692002@gmail.com>
2024-11-25 21:42:39 +0700

    fix

--- WebClient/Validation/ExamCreateRequestValidation.cs
+++ WebClient/Validation/ExamCreateRequestValidation.cs
@@ -17,10 +17,6 @@ namespace WebClient.Validation
                 .NotEmpty().WithMessage("Select subject for exam!")
                 .NotNull().WithMessage("Select subject for exam!");
 
-            this.RuleFor(v => v.CampusId)
-                .NotEmpty().WithMessage("Select campus review exam!")
-                .NotNull().WithMessage("Select campus review exam!");
-
             this.RuleFor(v => v.ExamDuration)
                 .NotEmpty().WithMessage("Select exam duration!")
                 .NotNull().WithMessage("Select exam duration!");
