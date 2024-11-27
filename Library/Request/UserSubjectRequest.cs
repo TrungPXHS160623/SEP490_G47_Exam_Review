a5cb36dea8a3a947f19c0994f3ca69d2858ae9f3
hunglt <hunglthe160235@fpt.edu.vn>
2024-11-27 10:20:58 +0700

    update

--- Library/Request/UserSubjectRequest.cs
+++ Library/Request/UserSubjectRequest.cs
@@ -5,7 +5,6 @@ namespace Library.Request
     public class UserSubjectRequest : UserRequest
     {
         public int? FacultyId { get; set; }
-        public IEnumerable<FacutyResponse> FacutyResponse { get; set; } = new List<FacutyResponse>();
-        public IEnumerable<SubjectResponse> SubjectResponses { get; set; } = new List<SubjectResponse>();
+        
     }
 }
